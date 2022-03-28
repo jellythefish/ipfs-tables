217fa8133050bb97998e7a652f201eec2f8663e2
Svyatoslav Krasnov <beatcazzze@gmail.com>
2022-03-28 16:01:52 +0300

    add some scripts

---
+++ scripts/configure_ipfs_node.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget https://dist.ipfs.io/go-ipfs/v0.7.0/go-ipfs_v0.7.0_linux-amd64.tar.gz
+tar -xvzf go-ipfs_v0.7.0_linux-amd64.tar.gz
+cd go-ipfs
+sudo bash install.sh
+sudo su
+cat <<EOT >> /lib/systemd/system/ipfs.service
+[Unit]
+Description=IPFS daemon
+After=network.target
+[Service]
+User=ipfsuser
+ExecStart=/usr/local/bin/ipfs daemon
+[Install]
+WantedBy=multiuser.target
+EOT
+exit
+ipfs init
+sudo systemctl start ipfs
+sudo systemctl enable ipfs
---
+++ scripts/connect_other_nodes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ipfs swarm connect /ip4/10.128.0.22/tcp/4001/ipfs/12D3KooWNbaSY3nV3AmgXqCb46LyHJzJJruANvuChLFqU8niUdec
+ipfs swarm connect /ip4/10.128.0.5/tcp/4001/ipfs/12D3KooWMttAoXBdn3XHkrGnVM2ADd4uxktdM96NbkSJke933tf4
+ipfs swarm connect /ip4/10.128.0.17/tcp/4001/ipfs/12D3KooWPURQdysCfzx2f23MPGVA9V6Z67YqDfUEnJBXc2h7BB8y
